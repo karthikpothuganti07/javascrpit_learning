eb0a881fe5c599acb5c9085f59abec806196d1ea
karthikpothuganti07 <mr.karthik.pothuganti@gmail.com>
2024-10-17 21:39:05 +0530

    learning

---
+++ quote.js
@@ -0,0 +1,376 @@
+// var wether="summer"
+// console.log(wether)
+
+// var wether="winter"
+// console.log(wether)
+
+// let god="shiva"
+// console.log(god)
+
+// god="krishna"
+// console.log(god)
+
+// const place='himachal'
+// console.log(place)
+//  place='ladakh'
+// console.log(place)
+
+// document.write("this is java script text meassage")
+// document.write("<h1>this is java script text meassage<h1>")
+
+// let sampleview=null;
+// console.log(sampleview)
+
+// let fruits=["apple","mango",55,{name:"kalyan"}]
+// console.log(fruits)
+// console.log(Array.isArray(fruits));
+
+// const games={
+// criket:"dhoni",
+// football:"ronaldo",
+// musican:"rehaman"
+// }
+
+// console.log(games);
+
+// console.log(typeof games);
+// console.log(Array.isArray(games));
+
+// var a=20;
+// a++;
+// var b=30;
+// b--
+
+// document.write("result " ,a ,b);
+
+// var movie="dashavataram"
+// let hero="kamal_hasan"
+// const tennis="saniya"
+
+// if (movie=="dashaataram"){
+//     document.write("hero is ",hero)}
+// // else
+// //     {
+// //         document.write("sorry wrong answer")
+// //     }
+// else if (tennis=="aniya"){
+//     document.write("best hot player is  ",tennis)
+// }
+
+// else{
+//     document.write("pora pumka  ")}
+
+
+// var das=80
+// var ram=35
+// var lax=27
+
+// if(das!==ram){
+//     document.write("hes the oldest man")}
+// else if (ram<lax){
+//     document.write("ram is big brother")}
+
+// else if (lax>das){
+//     document.write("lax is  son of das")
+// }
+// else{
+//     document.write("andahru erriukule")
+// }
+
+
+// var x=100;
+// var y=50;
+// var z=200;
+
+// if (x !==y && y<z){
+//     document.write("right answer");
+// }else if ((x+y)<z) {
+//     document.write("bahubali");
+// }
+
+// if ((x>z) || (y>z)){
+//     document.write("gopi")
+// }
+// else
+//  { document.write("sadvu radhu niku ")}
+
+
+// a=2+3==7?"correct":"wrong"
+
+// document.write(a)
+
+// console.log(a)
+
+// var adult = confirm ("are you above 18")
+
+// if (adult) {
+//     let age=prompt("please enter your age");
+//     document.write("this page displays above 18+content so your age is " +age+ " yuppy ur adult")
+// }
+
+// else {
+//     let name=prompt("pelase mention ur name");
+//     document.write("orey chinna manaki avasaramara "+name+" chadvuko po")
+
+// }
+
+// let  item = prompt("what do u want to order");
+// let  price= prompt("enter price");
+// let quant=prompt("how many plates");
+// let total=(price*quant);
+// switch(true){
+//     case total>=100000 && total<=20000000:
+//         result="too expensive"
+//     break;
+//     case total>=20000 && total<=100000:
+//         result="malli alochinchuko"
+//     break;
+//     case total>=10000 && total<=20000:
+//         result="some how manged"
+//     break;
+//     case total>=1000 && total<=10000:
+//         result="happy to eat kanivandi ra"
+//     break;
+//     case total>=100 && total<=1000:
+//         result="kukkalu kuda ravu e money tho"
+//     break;
+//     case total>=10 && total<=100:
+//         result="pora  pandhi malli ninnu dawath ante cheppu tisukono kottu"
+//     break;
+//     case total<10:
+//         result="sachipooora "
+//     break;
+// }
+
+// document.write( "hi "+  item +"your order price is "+ price+ " required quantity is  "
+//     +quant+" i am happy to announce that your total  value is",total,"so","<h2>"+result+"</h2>")
+// document.write(result);
+
+
+// let name =prompt("enter your name");
+// let income =prompt("enter your salary");
+
+// switch(true){
+//     case income >100000 && income<=1000000:
+//          result="your well settled"
+//         break;
+//     case income >80000 && income<=70000:
+//          result="you have bright future"
+//         break;
+//     case income >40000 && income<=50000:
+//          result="your struggler"
+//         break;
+//     case income >30000 && income<=20000:
+//          result="your poor"
+//         break;
+//     case income >10000 && income<=15000:
+//          result="your have  to die"
+//         break;
+//     case income<=1000:
+//          result="rip him self"
+//         break;
+//     default:
+//         result = result
+//       break
+
+// }
+
+// document.write(result)
+
+
+
+// var rider=confirm("are you a joker")
+// if (rider){
+//     let batman=prompt("i love batman joker")
+//     document.write("your true batman lover ",rider)
+// }
+
+// else{
+//     let spyder=prompt("i love spyder")
+//     document.write("your true spyder lover ",spyder)
+// }
+
+
+// let 
+
+// var name=prompt("enter your name ")
+// var age=prompt("enter your age")
+// var sweets=confirm("are you free from diabaties")
+//     if (sweets){
+//         let ghee=prompt("please take our ghee sweets")
+//         document.write("Hi ",name+" your age  ",+age+sweets," your order is perepring")
+//     }
+//     else{
+//         let dont=prompt("rey pumka padnhi  avtav ra")
+//         document.write("rey ",name, " ne vayassu ",age,"vaddu ra ante kuda " ,dont,"  ani cheptav endi ra lawdey"," tinak dengey")
+//     }
+
+
+
+// function karthik(){
+//     document.write("ayya namaskaram")
+// }
+
+// karthik()
+
+
+// var x="red";
+// let y="white";
+// const z="mixture "
+
+
+// function nams() {
+//     if(10>5) {
+//         var name="sunny"
+//         let sports="kabbaddi"
+//         const player="jhony"
+
+//         document.write(sports+"<br>")
+//         document.write(player+"<br>")
+        
+//     }
+//     document.write(name+"<br>")
+   
+   
+// }
+
+
+// nams()
+
+// function actors(x,y,z){
+//     return document.write(" i like "+x+" i hate "+y+" no felings for "+z)
+// }
+
+// actors("rajini","sharuk","uppalbalu")
+
+// var viras=prompt("enter your name")
+
+//       function ramayan(lok){
+//         return document.write("ravana is dead " +lok )
+//       }
+
+//       ramayan(viras)
+
+
+// let market=["fish","prawns",20,true,'amir',"kiran"]
+
+// market.push("biryani","rice",20)
+
+// document.write(market+"<br>")
+// document.write(market.length+"<br>")
+
+// let cds=["vikramrkudu","magadhira","ega"];
+
+// cds.pop()
+
+// document.write(cds)
+
+// var school=["pranya",10,"karthik",20,30,30,40,50,60,"raja","ram"]
+// var bigschool=school.join("")
+// document.write(bigschool)
+
+
+// let raje=["kar","bar","lar",1,2,3,4]
+// let kuj=raje.splice(2,1,"venaktesh")
+// document.write(raje +"<br>")
+// document.write(kuj)
+// console.log(kuj)
+
+// var raj = ["sunny","mia","dani","myra"];
+// var mycelb = raj.indexOf("kelly")
+
+// if (mycelb === -1) {
+//     raj.push("kelly")
+//     }
+
+// document.write(raj)
+
+// let raj=[10,20,30,40,50];
+// let kaj=["raj","kaj","laj","boj"]
+// let doj=[raj,kaj]
+
+// raj.forEach(function (items,index){
+//     document.write(index+1+")"+items+"<br>")
+// })
+
+// kaj.forEach(function(love,index,){
+//     document.write(index+1+")"+love+"<br>")
+// })
+
+
+// doj.forEach(function(dove,index,){
+//     document.write(index+1+" )"+dove)
+// })
+
+
+// var raj=["sunitha","ara","bava"]
+//  document.write(raj[1])
+
+
+//  var kaj=[
+//    [
+//     ["sama","boja","redd","lalla"],
+//       [1,2,3,4,5,6],
+//      ["q","r","s","t"]
+// ],
+//     ["rama","soma","bama","doma"],
+//     ["lol",1,100,{ram:"sitha"}],
+//  ]
+
+// document.write(kaj[0][2][2])
+
+// var kar = "i am worlds richest perosn"
+// var b="am worlds richest perosn"
+
+//  document.write(kar.split(""))
+// console.log(kar.split(""))
+
+// var ka= "   lod    "
+// document.write(ka)
+// console.log(ka.trim())
+
+// var lok="minh k "
+// document.write(lok.replace(5));
+
+
+// const text="karthik is a dangerous vilan";
+// const replaced=text.replace(/dangerous/,"great");
+// document.write(replaced)
+// console.log(replaced);
+
+// let text="raja the great"
+// document.write(text.toUpperCase()+"<br>"); 
+
+// let raja="RAJA THE GREAT"
+// document.write(text.toLowerCase());
+
+// for(var k=50; k>0; k--){
+//     document.write(k+")"+ "i love u karthik"+"</br>")
+
+// let kar = 0;
+// while (kar <= 20) {
+//     document.write(kar + ")" +"hello mr pk"+"<br>")
+//     kar++
+// }
+
+// var amount=500
+// var days=60
+// var intrest= 2
+
+// for (let i=0; i<=days; i++){
+//     if (i%8===0){
+//         amount+=intrest
+//     }
+//     document.write("yourname"+i+"thday"+amount+"<br>")
+// }
+    
+
+
+let number=[1,1,1,2,2,2,3,3,3,4,3,4,4,4,5,5,5,6,6,6,7,7,7,7,7]
+let mynumber=function removeDuplicates(number){
+    return[new set (number)];
+}
+
+console.log(removeDuplicates(number));
+
+
